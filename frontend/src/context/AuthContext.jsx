01c8f0f3e4f8d7e09ec116f64be4e49a59f863f2
ttkien <ttkien2004@gmail.com>
2025-05-31 21:19:45 +0700

    Add context. hook or frontend

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useReducer, useEffect } from "react";
+
+// create context
+export const AuthContext = createContext();
+
+// reducer
+export const authReducer = (state, action) => {
+	switch (action.type) {
+		case "LOGIN":
+			return { user: action.payload };
+		case "LOGOUT":
+			return { user: null };
+		default:
+			return state;
+	}
+};
+
+// context provider
+export const AuthContextProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(authReducer, {
+		user: null,
+	});
+	// useEffect(() => {
+
+	// })
+	return (
+		<AuthContext.Provider value={{ ...state, dispatch }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
